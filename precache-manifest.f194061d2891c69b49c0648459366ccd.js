43cd7b694b9605a728778c0d8ba685c86f4e6046
Harsha <Z1856779@students.niu.edu>
2020-05-25 17:17:44 -0500

    Updates

---
+++ precache-manifest.f194061d2891c69b49c0648459366ccd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af497458cc50ced1dcc5935fba79c1da",
+    "url": "/React_App/index.html"
+  },
+  {
+    "revision": "78b161502b55897fcdd7",
+    "url": "/React_App/static/css/main.0dc35b3c.chunk.css"
+  },
+  {
+    "revision": "cde407b717941bf34fd2",
+    "url": "/React_App/static/js/2.f1e25b38.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/React_App/static/js/2.f1e25b38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b161502b55897fcdd7",
+    "url": "/React_App/static/js/main.82e9e60c.chunk.js"
+  },
+  {
+    "revision": "808a93234432a975339e",
+    "url": "/React_App/static/js/runtime-main.c96c8c71.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/React_App/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/React_App/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/React_App/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/React_App/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/React_App/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/React_App/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/React_App/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/React_App/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/React_App/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
